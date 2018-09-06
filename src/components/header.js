7075b68839678e0c88fbdd01778e540b28d7ed2c
JunaYa <ayac3j@gmail.com>
2018-09-06 22:11:01 +0800

    update header layout & media

--- src/components/header.js
+++ src/components/header.js
@@ -1,30 +1,15 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import './header.css';
 
-const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+const Header = ({}) => (
+  <div className="header">
+    <div className="headerGroup">
+      <Link to="/"><button>Ulaqab</button></Link>
+      <Link to="/courses"><button>Courses</button></Link>
+      <Link to="/download"><button>Download</button></Link>
+      <Link to="/workshop"><button>Workshop</button></Link>
+      <Link to="/buy"><button>Buy</button></Link>
     </div>
   </div>
 )
